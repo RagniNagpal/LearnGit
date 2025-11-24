d0777df64efc73ef747f6476d70b140f0c91e37d
RagniNagpal <ragninagpal@gmail.com>
2025-11-24 15:05:07 +0530

    Model added

--- model/UserModel.js
+++ model/UserModel.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-  email: { type: String, required: true },
-  password: { type: String, required: true },
+  email: { type: String, require: true },
+  password: { type: String, require: true },
   firstName: String,
   lastName: String,
 });
