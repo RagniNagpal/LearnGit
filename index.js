5cc8316260371c71ae4727a23db91a8ffb37cf55
RagniNagpal <ragninagpal@gmail.com>
2025-11-24 14:55:32 +0530

    Status of project

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+mongoose.connect("mongodb://127.0.0.1:27017/test")
+    .then(() => {
+        console.log("DB connection successful");
+    })
+    .catch((err) => {
+        console.error("DB connection error:", err);
+    });
+
+    app.get("/",(req,res)=>{
+      res.json({
+        message : "ok"
+        
+      })
+    })
+app.listen(3000, () => {
+    console.log("Server started on port 3000");
+});
