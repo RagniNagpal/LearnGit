e17fb437b95ad442a5e6a90ff96d989a82604d44
RagniNagpal <ragninagpal@gmail.com>
2025-11-25 14:35:01 +0530

    Created user routes

---
+++ routes/user.router.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const router = express.Router(); // 
+// GET /getBlogs
+router.get("/getUsers", (req, res) => {
+  res.json({
+    success: true,
+    data: [
+      { title: "Name", para: "This is the content of my blog." }
+    ]
+  });
+});
+
+export default router;
