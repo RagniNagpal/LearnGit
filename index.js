94742b3fbf2248d489ed95357414d37848e5ab00
RagniNagpal <ragninagpal@gmail.com>
2025-11-25 14:50:12 +0530

    Merge feat/blog_routes into main, resolved conflicts

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
+app.use("/api/users",require("./routes/user.router"))
+app.use("/api/blogs",require("./routes/blog.router.js"))
 mongoose.connect("mongodb://127.0.0.1:27017/test")
     .then(() => {
         console.log("DB connection successful");
